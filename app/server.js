e93d3df854e043c87ed2daff3f2760b574a25bd9
Leonardo antonio <leleo@email.com>
2026-09-10 16:01:59 -0300

    fix: remove server.js file and its console log message

--- app/server.js
+++ app/server.js
@@ -1,3 +1,5 @@
 const app = require("./src/app");
 
+console.log("Versão nova em produção!");
+
 app.listen(3000, '0.0.0.0');
--- server.js
+++
@@ -1,5 +0,0 @@
-const app = require("./src/app");
-
-console.log("Versão nova em produção!");
-
-app.listen(3000, '0.0.0.0');
